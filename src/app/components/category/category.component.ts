7b46f9d334ca8d29fe14592d7e0588596ea6c5e6
Elena Trehub <elenatrehub1@gmail.com>
2018-10-15 23:23:35 +0300

    CategoryCommit

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit, ViewChild} from '@angular/core';
 import {Category} from '../../models/category/Category';
 import { FormControl , Validators } from '@angular/forms';
-import {MatChipInputEvent, MatInput} from '@angular/material';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+import { DeleteCategoryModalComponent } from '../../modals/deleteCategory.modal/deleteCategory.modal.component';
+import { CategoryData } from '../../models/modal.category/category.data';
+import { MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-category',
@@ -10,17 +13,25 @@ import {MatChipInputEvent, MatInput} from '@angular/material';
 })
 export class CategoryComponent implements OnInit {
 
+  displayedColumns: string[] = ['id', 'title', 'update', 'delete'];
+  dataSource: MatTableDataSource<Category>;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+
+
   public categories: Category[];
   public cat: Category = new Category(-1, '');
   public visib: boolean;
 
   public categoryNameFormControl = new FormControl('', [
     Validators.required,
-    Validators.pattern(/^[a-z][0-9]{2,20}$/i),
+    Validators.pattern(/^[a-z,а-я,0-9, ]{2,20}$/i),
   ]);
 
 
-  constructor() {
+  constructor(public dialog: MatDialog) {
 
     this.categories = [
       new Category(1, 'Аксессуар')
@@ -29,22 +40,73 @@ export class CategoryComponent implements OnInit {
     ];
     this.visib = false;
 
+    this.dataSource = new MatTableDataSource(this.categories);
+
   }
 
   ngOnInit() {
-
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
 
   }
 
   addForm(){
+    this.cat.categoryTitle = '';
     this.visib = !this.visib;
   }
 
-  addCategory(){
-    this.visib = false;
+  addCategory(event){
+
+    if ( this.categoryNameFormControl.hasError('required') || this.categoryNameFormControl.hasError('pattern')){
+      return;
+    }
+
+    if ( this.categoryNameFormControl.hasError('required') ){
+      return;
+    }
+
+    if ( event instanceof KeyboardEvent && event.code === 'Enter' ){
+      this.visib = false;
+    }
+    else if ( event instanceof  MouseEvent){
+      this.visib = false;
+    }
+
+
+
+
   }
 
+  delete( event ){
 
+
+
+    const categorData: CategoryData = new class implements CategoryData {
+      categoryID: number;
+      categoryTitle: string;
+    };
+
+    categorData.categoryID = 1;
+    categorData.categoryTitle = event.target;
+
+    if ( event instanceof KeyboardEvent && event.code === 'Enter' ){
+      this.openDialog(categorData);
+    }//if
+    else if ( event instanceof  MouseEvent){
+      this.openDialog(categorData);
+    }//else if
+
+
+  }
+
+  openDialog(catData: CategoryData ): void {
+
+    const dialogRef = this.dialog.open(DeleteCategoryModalComponent, {
+      width: '400px',
+      data: catData
+    });
+
+  }
 
 
 }
---
+++ src/app/models/modal.category/category.data.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+export interface CategoryData {
+
+  categoryTitle: string;
+  categoryID: number;
+}
